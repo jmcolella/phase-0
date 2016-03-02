448d244e6be6c5c387b248bf30069d3c4e7d4c5c
jmcolella <colella.john@gmail.com>
2016-03-01 19:56:10 -0500

    complete all challenges for module 4.3

---
+++ week-4/math/my_solution.rb
@@ -0,0 +1,27 @@
+# Math Methods
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+
+def add(num_1, num_2)
+ return num_1 + num_2
+end
+
+def subtract(num_1, num_2)
+ return num_1 - num_2
+end
+
+def multiply(num_1, num_2)
+ return num_1 * num_2
+end
+
+def divide(num_1, num_2)
+ return (num_1 / num_2).to_f
+end
+
+add(2,3)
+subtract(2,3)
+multiply(2,3)
+divide(2,3)
