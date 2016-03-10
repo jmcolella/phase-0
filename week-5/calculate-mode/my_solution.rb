ea4298811601c3c69184389ec46ad58a4fed9724
jmcolella <colella.john@gmail.com>
2016-03-10 15:30:52 -0500

    complete mod 5.2 and 5.3

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -12,19 +12,33 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? an array of numbers or strings
+# What is the output? (i.e. What should the code return?) an array of the most frequent values from the input array
 # What are the steps needed to solve the problem?
+# 1. Create a blank hash
+# 2. Iterate through the input array such that each index in the array is a key and the default value is one, unless that index value appears again and the value will increase by one
+# 3. Iterate through the newly created hash and grab the keys that have the highest total value
+# 4. Return the keys found in step 3 as an array
 
 
 # 1. Initial Solution
 
+def mode(array)
+  mode_hash = Hash.new(0)
+  array.each do |num|
+    mode_hash[num] +=1
+  end
+  return_array = mode_hash.select {|num, freq| freq == mode_hash.values.max}.keys
+  p return_array
+end
+
+# find_mode([1,2,3,3,3,4,5,5,5,])
 
 
 
 # 3. Refactored Solution
 
-
+#
 
 
 # 4. Reflection
--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -14,23 +14,48 @@
 
 # 0. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
+# What is the input? An array, minimum size, value
+# What is the output? (i.e. What should the code return?) a new array with padding
 # What are the steps needed to solve the problem?
+# IF minimum size is less than the length of the array, return the array
+# ELSE minimum size is greater than the array
+# Subtract the array size from the minimum size and store in a variable "length to add"
+# Make a new array with the size as length to add and the value as value.
+#Concatenate two arrays
+#Return the new array
 
 
 # 1. Initial Solution
-def pad!(array, min_size, value = nil) #destructive
-  # Your code here
+
+# NEED TO MAKE THIS NON DESTRUCTIVE
+
+def pad(array, min_size, value = nil)
+  arr_var = array
+  if arr_var.length >= min_size
+  return arr_var
+  elsif arr_var.length < min_size
+  length_to_add = min_size - arr_var.length
+  new_array = arr_var + Array.new(length_to_add,value)
+ end
+  return new_array.flatten
 end
 
-def pad(array, min_size, value = nil) #non-destructive
-  # Your code here
+
+def pad!(array, min_size, value = nil)
+  arr_var = array
+  if arr_var.length >= min_size
+  return arr_var
+  elsif arr_var.length < min_size
+  length_to_add = min_size - arr_var.length
+  array_to_add = Array.new(length_to_add,value)
+  new_array = arr_var << array_to_add
+ end
+  return new_array.flatten!
 end
 
 
 # 3. Refactored Solution
 
-
+# There are no methods that could simplify our initial solution.
 
 # 4. Reflection
