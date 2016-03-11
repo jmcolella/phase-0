e2b863055aec6a67993ddbae182bcefcbb9470c1
jmcolella <colella.john@gmail.com>
2016-03-10 21:56:01 -0500

    complete reflections and refactor for mod 5.2 and 5.3

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -28,17 +28,58 @@ def mode(array)
   array.each do |num|
     mode_hash[num] +=1
   end
+  return_array = []
+  mode_hash.each do |num, freq|
+    if freq == mode_hash.values.max
+      return_array << num
+    end
+  end
+  p return_array
+end
+
+# 3. Refactored Solution
+
+def mode(array)
+  mode_hash = Hash.new(0)
+  array.each {|num| mode_hash[num] +=1}
   return_array = mode_hash.select {|num, freq| freq == mode_hash.values.max}.keys
   p return_array
 end
 
-# find_mode([1,2,3,3,3,4,5,5,5,])
-
-
-
-# 3. Refactored Solution
-
-#
-
 
 # 4. Reflection
+
+# Which data structure did you and your pair decide to implement and why?
+
+# We decieded to convert the input array into a hash because we figured that would be the best way to consolidate any
+# repeated numbers from the array and make those keys, and then the frequency those numbers appear would be the corresponding
+# values. Then it would just be a matter of finding out which of the values in the hash is the maximum and the print out
+# the correspond key as part of an array.
+
+# Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+
+# Again, this one we just pseudocoded by breaking down the problem into general steps to help guide us through the nitty
+# gritty of the Ruby code. I would say this led to mixed results. Our general steps helped with formulating how to change
+# the input array into a hash, but then we couldn't figure out from our general steps how to then pull the highest values
+# and assign their corresponding keys to an array. It was a frustrating process that we had to do a little research to
+# get through.
+
+# What issues/successes did you run into when translating your pseudocode to code?
+
+# Biggest issue was iterating over the newly created hash to find the values with the highest number and then return those
+# values' associated key as an array. We used an each method to iterate across both key and values in the hash, but we had
+# to look up a particular method to get the max integer of all the values since we were having trouble coming up with a
+# way to do that otherise (any hints would be welcome). We then went through many different iterations of how to return
+# the max key values as an array, and we tried to move pieces inside and out of the each iterator, changing variables,
+# concatenating arrays (or what we thought we arrays). Eventually, we realized that we were assigning the value variable
+# in our each iterator to the max value, rather than using the "==" comparison operator. Once we got that, everything fell
+# into place.
+
+# What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they
+# difficult to implement?
+
+# Each was a key method for iterating across the newly created hash. The values.max method was instrumental in helping us
+# find the maximum values in the hash. We then found this cool method keys, which we added to the end of our each code
+# block. This method keys just returns the key values from a hash as an array. So, yeah, that basically did exactly what
+# we needed it to do, and combined with our block to located the max values in the hash, it allowed us to efficiently
+# return an array of just those key values.
--- week-5/pad-array/my_solution.rb
+++ week-5/pad-array/my_solution.rb
@@ -14,31 +14,37 @@
 
 # 0. Pseudocode
 
-# What is the input? An array, minimum size, value
-# What is the output? (i.e. What should the code return?) a new array with padding
+# What is the input?
+# An array, minimum size, value
+
+# What is the output? (i.e. What should the code return?)
+# a new array with padding
+
 # What are the steps needed to solve the problem?
 # IF minimum size is less than the length of the array, return the array
 # ELSE minimum size is greater than the array
 # Subtract the array size from the minimum size and store in a variable "length to add"
 # Make a new array with the size as length to add and the value as value.
-#Concatenate two arrays
-#Return the new array
+# Concatenate two arrays
+# Return the new array
 
 
 # 1. Initial Solution
 
-# NEED TO MAKE THIS NON DESTRUCTIVE
-
 def pad(array, min_size, value = nil)
-  arr_var = array
-  if arr_var.length >= min_size
-  return arr_var
-  elsif arr_var.length < min_size
-  length_to_add = min_size - arr_var.length
-  new_array = arr_var + Array.new(length_to_add,value)
- end
-  return new_array.flatten
+  clone_array = array.clone
+  if clone_array.length >= min_size
+    return clone_array
+  elsif clone_array.length < min_size
+    length_to_add = min_size - clone_array.length
+    new_array = clone_array + Array.new(length_to_add,value)
+  end
+  p new_array.flatten
 end
+
+array = [1,2,3]
+pad(array, 5, "apples")
+p array
 
 
 def pad!(array, min_size, value = nil)
@@ -46,9 +52,9 @@ def pad!(array, min_size, value = nil)
   if arr_var.length >= min_size
   return arr_var
   elsif arr_var.length < min_size
-  length_to_add = min_size - arr_var.length
-  array_to_add = Array.new(length_to_add,value)
-  new_array = arr_var << array_to_add
+    length_to_add = min_size - arr_var.length
+    array_to_add = Array.new(length_to_add,value)
+    new_array = array << array_to_add
  end
   return new_array.flatten!
 end
@@ -59,3 +65,44 @@ end
 # There are no methods that could simplify our initial solution.
 
 # 4. Reflection
+
+# Were you successful in breaking the problem down into small steps?
+
+# I believe my pair and I were succesfuly at breaking the problem into semi-small steps. As you can see from our
+# pseudocode, each step is a breakdown of what our code ended up being, but they are kind of general and don't really
+# get out the process of how to accomplish what that step describes. I'm still a little hazy on what pseudocode can be,
+# so maybe these general steps are good enough and you can get into the nitty-gritty when actually writing the code.
+# Some feedback on how you (review) go about pseudocode would be nice, thanks.
+
+# Once you had written your pseudocode, were you able to easily translate it into code? What difficulties and successes did
+# you have?
+
+# I was surprised how easily we were in the end able to get the general steps we outlined into working code. The big
+# difficulty we had was trying to get the pad method to be non-destructive; that tok a lot of tinerking and experimenting.
+
+# Was your initial solution successful at passing the tests? If so, why do you think that is? If not, what were the errors you encountered and what did you do to resolve them?
+
+# Our initial solution was not successful at passing all of the tests. We got hung up on the "pad operates non
+# destructively" test. We actually couldn't figure out how to do this while we were still paired up, so we had to do some
+# research on our own. I eventually found this clone method that can be called on any object in Ruby. You can then assign
+# a new variable to this clone object and use the new variable to manipulate the information from the original object
+# without actually changing the origin object. It took a long time for me to figure that one out, glad I did though.
+
+# When you refactored, did you find any existing methods in Ruby to clean up your code?
+
+# Our initial solution was written in a way that we felt could not be simplified using any known Ruby methods.
+
+# How readable is your solution? Did you and your pair choose descriptive variable names?
+
+# We tried hard to use the same argument names as the variable names throughout the method. The only new variable we
+# introduced was a "new_array" where we stored the two concatenated arrays. That array name may be a bit non-descript,
+# but for the purposes of this small challenge, I think it works.
+
+# What is the difference between destructive and non-destructive methods in your own words?
+
+# A destructive method is one that permanently changes the object to which it is called. A non-destructive method is one
+# that does not permanently change the object to which it is called. For example, if we call the sort method with a breaking
+# operator (!), to denote destructive, on an array, then however that array is sorted will be the new order of the array,
+# completely replacing the original version of the array. On the other hand, if we were to call the sort method on the array
+# without a bang operator, then the new sorted array will not replce the original array the method was called on, the
+# sort method would create two separate objects, the original and the one that was sorted.
