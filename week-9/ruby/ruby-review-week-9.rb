a96d1cc8118b27ebfd4a3f5b20724bce122d82ac
jmcolella <colella.john@gmail.com>
2016-04-05 12:27:27 -0400

    attempt fibonacci challenge, will return later

---
+++ week-9/ruby/ruby-review-week-9.rb
@@ -0,0 +1,44 @@
+# I worked on this challenge [by myself, with: ].
+# This challenge took me [#] hours.
+
+# Pseudocode
+# Input: number
+# Output: true or false if input number is part of fibonacci sequence
+# Steps:
+# create a start point of 0
+# create a counter up that adds on top of itself
+
+
+# Initial Solution
+
+def is_fibonacci?(num)
+  counter = 0
+  fib = ((((1+(Math.sqrt(5)))/2)**counter) - (-(1+(Math.sqrt(5)))/2)**(-counter)) / (Math.sqrt(5))
+  until counter == num
+    if fib == num
+      p true
+    else
+      p false
+    counter += 1
+    end
+  end
+end
+
+# def fibonacci(num)
+#   actual = num - 1
+#   fib = ((((1+(Math.sqrt(5)))/2)**actual) - (-(1+(Math.sqrt(5)))/2)**(-actual)) / (Math.sqrt(5))
+#   p fib.to_i
+# end
+
+is_fibonacci?(3)
+# Refactored Solution
+
+# 0,1,1,2,3,5,8
+#
+# p Math.sqrt(5)
+
+
+
+
+
+# Reflection
