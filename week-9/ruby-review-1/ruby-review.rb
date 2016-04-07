a6a954b959b4faf847a89ad274f86df295860a3a
jmcolella <colella.john@gmail.com>
2016-04-07 14:28:04 -0400

    add reflection to module 9.5 file

--- week-9/ruby-review-1/ruby-review.rb
+++ week-9/ruby-review-1/ruby-review.rb
@@ -73,10 +73,6 @@ goodcard = CreditCard.new(4563960122001999)
 # badcard = CreditCard.new(4444311)
 badcard2 = CreditCard.new(1111111111111111)
 
-#goodcard.double
-#goodcard.sum
-#true_test = goodcard.check_card
-
 def assert
   raise "Assertion failed!" unless yield
 end
@@ -86,3 +82,24 @@ assert {badcard2.check_card}
 
 
 # 5. Reflection
+
+# What concepts did you review in this challenge?
+
+# We reviewed how to manipulate arrays through iteration (the reverse words challenge) and how to create our own test statements
+# (with this assert challenge). It was good practice to figure out how one can create a basic test method that will raise an
+# error unless the block passed to it is true. I hope to make use of this basic structure as we move into Phase 1.
+
+# What is still confusing to you about Ruby?
+
+# I'm still a bit confused on how some methods can be destructive and others not. I understand the idea of the bang (!), but, for
+# the CreditCard class above, when running our tests, we realized that something in that class was destructive to the input; in
+# other words, if you call the method check_card twice on the same object, the second time it is called the object uses the number
+# that was resulted from the first call of the method, so the second method call ultimately fails. We were unsure of why this class
+# was destructive to the input, and we didn't have a chance to explore that fully. I would like to have a better understanding of
+# that heading into Phase 1.
+
+# What are you going to study to get more prepared for Phase 1?
+
+# I want to re-read the first chapter of Well-Grounded Rubyist to get a better understanding of the general framework of Ruby;
+# when I read it the first time, everything kind of went over my head, so I'm hoping with the new knowledge I have, the chapter
+# will make more sense.
